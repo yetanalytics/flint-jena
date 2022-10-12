32d7a8cbaf7a81c85623e7960daa32879ebae55a
kelvinqian00 <kelvinqian2@gmail.com>
2022-10-12 14:14:49 -0400

    Add common AST methods

---
+++ src/main/com/yetanalytics/flint_jena/ast.clj
@@ -0,0 +1,23 @@
+(ns com.yetanalytics.flint-jena.ast
+  (:require [clojure.walk :as w]
+            [com.yetanalytics.flint.util :as u]))
+
+(defn ast-node-dispatch
+  ([_ ast-node]
+   (if-some [k (u/get-keyword ast-node)] k :default))
+  ([_ _ ast-node]
+   (if-some [k (u/get-keyword ast-node)] k :default)))
+
+(defmulti ast-node->jena
+  "Given the AST node `x`, return a Jena query/update builder instance."
+  ast-node-dispatch)
+
+(defmethod ast-node->jena :default [_ ast-node]
+  ast-node)
+
+(defn ast->jena
+  "Given an `opts` map and a conformed Flint `ast`, convert it into a
+   Jena instance or a coll thereof."
+  [opts ast]
+  (let [ast-node->jena* (partial ast-node->jena opts)]
+    (w/postwalk ast-node->jena* ast)))
